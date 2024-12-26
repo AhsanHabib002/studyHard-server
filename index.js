e073f8a083eb1177b1687cd9bd927f66587af4d9
Your NameAhsanHabib002 <ahsanhabib12300@gmail.com>
2024-12-26 06:42:18 +0600

    JWT setup

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
-
+app.use(cookieParser());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9fdmi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,6 +31,19 @@ async function run() {
       .db("StudyHard_database")
       .collection("assingments");
     
+    // auth api
+    app.post('/jwt', (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRECT, {expiresIn: '9h'});
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: false
+        })
+        .send({success: true})
+    })
+
+    //   All assignments Api
     app.get('/assignments', async(req,res)=>{
         const cursor = assignmentCollection.find();
         const result = await cursor.toArray();
