62cff40cc3c0b0ad4a1d86b6c1ff722b4bf39a3a
Your NameAhsanHabib002 <ahsanhabib12300@gmail.com>
2024-12-26 19:08:01 +0600

    My submission api

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-app.use(cors({
-    origin: ['http://localhost:5173'],
-    credentials:true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
 const verifyToken = (req, res, next) => {
-    const token = req.cookies?.token;
+  const token = req.cookies?.token;
 
-    if(!token){
-        return res.status(401).send({message: 'unauthorized access'});
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRECT, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
     }
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRECT, (err, decoded) => {
-        if(err){
-            return res.status(401).send({message: 'unauthorized access'});
-        }
-        req.user = decoded;
-        next();
-    })
-
-}
+    req.user = decoded;
+    next();
+  });
+};
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9fdmi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -50,48 +51,69 @@ async function run() {
     const assignmentCollection = client
       .db("StudyHard_database")
       .collection("assingments");
-    const submissionsCollection = client.db("StudyHard_database").collection("submissions")
-    
-    // auth api
-    app.post('/jwt', (req, res) => {
-        const user = req.body;
-        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRECT, {expiresIn: '9h'});
+    const submissionsCollection = client
+      .db("StudyHard_database")
+      .collection("submissions");
 
-        res.cookie('token', token, {
-            httpOnly: true,
-            secure: false
+    // auth api
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRECT, {
+        expiresIn: "9h",
+      });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
         })
-        .send({success: true})
+        .send({ success: true });
     });
-    app.post('/logout', (req, res) =>{
-        res.clearCookie('token',{
-            httpOnly: true,
-            secure: false
+    app.post("/logout", (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: false,
         })
-        .send({success: true})
-    })
+        .send({ success: true });
+    });
 
     //   All assignments Api
-    app.get('/assignments', async(req,res)=>{
-        const cursor = assignmentCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get("/assignments", async (req, res) => {
+      const cursor = assignmentCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     });
 
     // Assignments Details
-    app.get("/assignments/:id", verifyToken, async(req, res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-        const result = await assignmentCollection.findOne(query);
-        res.send(result);
+    app.get("/assignments/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await assignmentCollection.findOne(query);
+      res.send(result);
     });
 
     // Submissions Details APIS
-    app.post('/submissions',verifyToken, async(req, res) => {
-        const submissionData = req.body;
-        const result = await submissionsCollection.insertOne(submissionData);
-        res.send(result);
-    })
+    app.get("/mysubmission", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const query = { examinee: email };
+      const result = await submissionsCollection.find(query).toArray();
+      for (const submission of result) {
+        const query1 = { _id: new ObjectId(submission.submit_id) };
+        const result1 = await assignmentCollection.findOne(query1);
+        if (result1) {
+          submission.title = result1.title;
+          submission.marks = result1.marks;
+        }
+      }
+      res.send(result);
+    });
+
+    app.post("/submissions", verifyToken, async (req, res) => {
+      const submissionData = req.body;
+      const result = await submissionsCollection.insertOne(submissionData);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
